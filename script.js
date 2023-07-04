9250f0f4f4b3acc1f0d968dafd2caf03236d5f7e
Willie Alldredge <williealldredge@yahoo.com>
2023-07-03 22:17:18 -0700

    add playround function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function getComputerChoice() { 
     let choice = Math.random() * 100;
-    console.log(choice)
+    
     if (choice <= 33) {
         return 'rock';
     } else if (choice > 33 && choice < 67) {
@@ -10,5 +10,32 @@ function getComputerChoice() {
     } else {
         return 'scissors'
     }
-
 }
+
+//write function that plays a single round 
+
+ function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        return "you picked the same";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose, paper beats scissors';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win, rock beats scissors';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win, paper beats rock';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose, scissors beats rock';
+    }else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        return 'You lose, rock beats scissors';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win, scissors beats paper'
+    }
+ }
+
+const playerSelection = prompt('Rock, Paper, or Scissors?');
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
+console.log(playerSelection, computerSelection)
+
+
+
